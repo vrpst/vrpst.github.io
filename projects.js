23c7b14eacd86b719d2893cd3e2a06988c65c76f
vrpst <lucas.verpoest@gmail.com>
2024-11-10 01:09:23 +0000

    major upgrades (nav, restyle, etc.)

---
+++ projects.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    // flink
+    document.querySelector('#flink').addEventListener('click', function() {
+        window.location.href = "flink.html";
+    });
+
+    // plaza
+    document.querySelector('#plaza').addEventListener('click', function() {
+        window.location.href = "plaza.html";
+    });
+
+    // sudoku
+    document.querySelector('#sudoku').addEventListener('click', function() {
+        window.location.href = "sudoku.html";
+    });
+}

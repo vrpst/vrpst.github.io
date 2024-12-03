9260834303dcba28074ffbead87ba91be15d0d54
vrpst <lucas.verpoest@gmail.com>
2024-12-03 00:10:15 +0000

    add transitions!

---
+++ index.js
@@ -0,0 +1,9 @@
+window.setTimeout(function() {
+    document.getElementById('intro').className += ' faded-in-left me-fade'
+    }, 10)
+window.setTimeout(function() {
+    document.getElementById('about').className += ' faded-in-left'
+    }, 500)
+window.setTimeout(function() {
+    document.getElementById('projects').className += ' faded-in-left'
+    }, 800)
